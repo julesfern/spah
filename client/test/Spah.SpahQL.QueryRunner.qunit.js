bb68b868268fcc2028337002d35d68d7dfefe400
Dan Glegg <dan@angryamoeba.co.uk>
2011-06-10 17:25:35 +0100

    Added test server for running the qunit suite, which now relies on an async load for query fixtures (and so can't run over localhost). Moved query challenge/response fixtures to JSON files so they can be shared by the ruby parser (guess what i'm working on next)

--- client/test/Spah.SpahQL.QueryRunner.qunit.js
+++ client/test/Spah.SpahQL.QueryRunner.qunit.js
@@ -2,18 +2,49 @@ $(document).ready(function() {
   
   var qp = Spah.SpahQL.QueryParser;
   var qr = Spah.SpahQL.QueryRunner;
+  // Load fixtures
+  var queryData, queryTests;
   
-  module("Spah.SpahQL.QueryRunner");
+  module("Spah.SpahQL.QueryRunner", {
+    setup: function() {
+      console.log("Loading query data and fixtures...");
+      
+      $.ajax("/client/test/fixtures/queryData.json", {
+        dataType: 'json',
+        async: false,
+        success: function(data, textStatus, jqXHR) {
+          console.log("Loaded data fixture:", data);
+          queryData = data;
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+          console.log("Failed to load query data: ", textStatus, errorThrown, jqXHR);
+        }
+      });
+      
+      $.ajax("/client/test/fixtures/queryTests.json", {
+        dataType: 'json',
+        async: false,
+        success: function(data, textStatus, jqXHR) {
+          console.log("Loaded test fixture:", data);
+          queryTests = data;
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+          console.log("Failed to load query data: ", textStatus, errorThrown, jqXHR);
+        }
+      });
+      
+    }
+  });
   
   // error on #select with an assertion query
   
   // Run all selection queries
   test("Selection Query Fixtures: Runs the fixture set with the correct results", function() {
-    for(var s in Fixtures.Query.Selection) {
-      var qfix = Fixtures.Query.Selection[s];
+    for(var s in queryTests.Selection) {
+      var qfix = queryTests.Selection[s];
       var n = qfix.name;
       var q = qp.parseQuery(qfix.query);
-      var actual = qr.select(q, Fixtures.Query.Data);
+      var actual = qr.select(q, (qfix.data || queryData));
       var expected = qfix.result;
     
       equal(actual.length, expected.length, n+": Result count matched ("+jQuery.map(actual, function(result, index) {return [result.path, result.value]}).join(", ")+")");
@@ -29,11 +60,11 @@ $(document).ready(function() {
   
   // Run all assertion queries
   test("Assertion query fixtures: Runs the fixture set with the correct results", function() {
-    for(var s in Fixtures.Query.Assertion) {
-      var qfix = Fixtures.Query.Assertion[s];
+    for(var s in queryTests.Assertion) {
+      var qfix = queryTests.Assertion[s];
       var n = qfix.name;
       var q = qp.parseQuery(qfix.query);
-      var actual = qr.assert(q, Fixtures.Query.Data);
+      var actual = qr.assert(q, (qfix.data || queryData));
       var expected = qfix.result;
       equal(actual, expected, n+": Expected boolean returned ('"+qfix.query+"' -> "+expected+")");
     }
---
+++ server.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+require 'sinatra'
+
+set :public, File.join(File.dirname(__FILE__))
+
+get "/" do
+  redirect "/client/tests"
+end
+get "/client/tests" do
+  redirect '/client/test/all-tests.html'
+end
