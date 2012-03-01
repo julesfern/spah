a99c4cecb4ed71caff129fe6e57502186f3e347e
Dan Glegg <dan@angryamoeba.co.uk>
2012-03-01 17:53:50 +0000

    fleshing out the basic state model, hooks for reduction and expansion

--- src/Spah.State.js
+++ src/Spah.State.js
@@ -18,6 +18,18 @@ Spah.classExtend("Spah.State", Spah.SpahQL.QueryResult, {
     this.path = "/"; 
     this.value = data || {};
     this.sourceData = this.value;
+  },
+
+  /**
+   * Spah.State#apply(query, ruleFunc) -> Spah.State
+   * query (String): A SpahQL query limiting the results against which the rule will be run.
+   * ruleFunc (Function): A function to be applied to all matching SpahQL results. Takes an individual QueryResult as the only argument.
+   *
+   * Applies the given function to all matches for the given SpahQL query, leaving this
+   * instance unchanged and returning a new Spah.State instance with the changes applied.
+   **/
+  "apply": function(query, ruleFunc) {
+    
   }
   
 });
--- test/Spah.State.test.js
+++ test/Spah.State.test.js
@@ -7,6 +7,26 @@ exports["Spah.State"] = {
   	test.equals(state.path, "/");
   	test.ok(Spah.SpahQL.DataHelper.eq(state.value, data));
   	test.done();
+  },
+
+  "May have a query rule applied to it": function(test) {
+  	var data = {
+  		"foo": {"bar": "baz", "finangle": "patang"},
+  		"bar": "baz"
+  	};
+  	var expected = {
+  		"foo": {"bar": "baz2"},
+  		"bar": "baz2"
+  	};
+
+  	var state = new Spah.State(data);
+  	var modifiedState = state.apply("//bar", function(result) {
+  		result.replace("baz2");
+  	});
+
+  	test.ok(Spah.SpahQL.DataHelper.eq(data, state.value))
+  	test.ok(Spah.SpahQL.DataHelper.eq(expected, modifiedState.value));
+  	test.done();
   }
   
 };
